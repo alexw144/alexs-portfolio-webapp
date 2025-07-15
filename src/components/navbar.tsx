f3fc50ad41f1d73ce0e6325f791055441b32a99f
alexw144 <amwagner4472@live.carlow.edu>
2025-07-15 17:53:43 -0400

    Minor updates to the navbar component. Removed name and added 3 icons with working links to relevant websites.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,11 @@
+import LinkedInIcon from "../assets/icons/LI-In-Bug.png";
+import GitHubIcon from "../assets/icons/github-mark.png";
+import GitLabIcon from "../assets/icons/gitlab-logo-500.png";
+
 function NavItems() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-field">
-        <span className="navbar-brand">Alex Wagner</span>
         <a className="navbar-brand" href="#about">
           About
         </a>
@@ -14,6 +17,35 @@ function NavItems() {
         </a>
         <a className="navbar-brand" href="#contact">
           Contact
+        </a>
+      </div>
+      <div className="container-field">
+        <a
+          className="navbar-brand"
+          href="https://www.linkedin.com/in/alexander-m-wagner/"
+        >
+          <img
+            src={LinkedInIcon}
+            alt="Alex's Linkedin profile page"
+            className="nav-icon"
+          ></img>
+        </a>
+        <a className="navbar-brand" href="https://github.com/alexw144">
+          <img
+            src={GitHubIcon}
+            alt="Alex's GitHub profile and projects"
+            className="nav-icon"
+          ></img>
+        </a>
+        <a
+          className="navbar-brand"
+          href="https://gitlab.com/users/awagner50/projects"
+        >
+          <img
+            src={GitLabIcon}
+            alt="Alex's GitLab profile and projects"
+            className="nav-icon"
+          ></img>
         </a>
       </div>
     </nav>
