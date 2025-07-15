23c13f420d5f46c741d3833c1f74a057f7acb6a5
alexw144 <amwagner4472@live.carlow.edu>
2025-07-15 18:08:10 -0400

    Added basic information the the "about me" component

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -2,8 +2,16 @@ function AboutSect() {
   return (
     <section id="about">
       <div className="container-fluid">
-        <h2>About Me</h2>
-        <p>Something</p>
+        <h2>Hello, I'm Alex Wagner</h2>
+        <p>
+          I am an aspiring web developer, born and raised in Pittsburgh,
+          Pennsylvania. I paved the way as one of the first students to graduate
+          from Carlow University with a Bachelor's degree in Computer Science. I
+          even had the opportunity to create important webpages for their
+          website. I hope to one day become a full-stack developer, but for now,
+          I am looking to jumpstart my career as a web developer. You can catch
+          me creating new websites, playing video games, or studying Japanese.
+        </p>
       </div>
     </section>
   );
