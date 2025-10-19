c5564c2328202a2a5b29b3abdaba5b1de70479a0
alexw144 <[amwagner4472@live.carlow.edu])>
2025-10-19 17:27:21 -0400

    Changed navbar color. Added resume, github, linkedin, and gitlab to contact info section. Updated the github and linkedin icons. Created a documents folder in assets and added my resume. Continued to overhaul CSS and made site more responsive.

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -2,16 +2,45 @@ function ContactInfo() {
   return (
     <section className="section-contact" id="contact">
       <div className="container-fluid">
-        <h2>My Contact Information</h2>
+        <h2>Contact Information and Useful Links</h2>
         <ul>
           <li>Phone: 412-443-7785</li>
           <li>
             Email:{" "}
-            <a href="mailto:alexanderwagner144@gmail.com">
+            <a className="contact-a" href="mailto:alexanderwagner144@gmail.com">
               alexanderwagner144@gmail.com
             </a>
           </li>
+          <li>
+            Resume:{" "}
+            <a className="contact-a" href="src\assets\documents\Alexander Wagner Resume 1.pdf">
+              Alexander Wagner Resume 1.pdf
+            </a>
+          </li>
+          <li>
+            LinkedIn:{" "}
+            <a className="contact-a" href="https://www.linkedin.com/in/alexander-m-wagner/">
+              linkedin.com/in/alexander-m-wagner/
+            </a>
+          </li>
+          <li>
+            GitHub:{" "}
+            <a className="contact-a" href="https://github.com/alexw144">
+              github.com/alexw144
+            </a>
+          </li>
+          <li>
+            GitLab:{" "}
+            <a className="contact-a" href="https://gitlab.com/users/awagner50/projects">
+              gitlab.com/awagner50
+            </a>
+          </li>
         </ul>
+        <div className="contact-buttons">
+          <a className="button-link" href="https://www.linkedin.com/in/alexander-m-wagner/">LinkedIn</a>
+          <a className="button-link" href="https://github.com/alexw144">GitHub</a>
+          <a className="button-link" href="https://gitlab.com/users/awagner50/projects">GitLab</a>
+        </div>
       </div>
     </section>
   );
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,50 +1,68 @@
 import LinkedInIcon from "../assets/icons/LI-In-Bug.png";
 import GitHubIcon from "../assets/icons/github-mark.png";
 import GitLabIcon from "../assets/icons/gitlab-logo-500.png";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function NavItems() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-field">
-        <Link className="navbar-brand" to="/">
-          Home
-        </Link>
-        <Link className="navbar-brand" to="/school-projects">
-          School Projects
-        </Link>
-        <Link className="navbar-brand" to="/work-experience">
-          Work Experience
-        </Link>
-      </div>
-      <div className="container-field">
-        <a
-          className="navbar-brand"
-          href="https://www.linkedin.com/in/alexander-m-wagner/"
-        >
-          <img
-            src={LinkedInIcon}
-            alt="Alex's Linkedin profile page"
-            className="nav-icon"
-          ></img>
-        </a>
-        <a className="navbar-brand" href="https://github.com/alexw144">
-          <img
-            src={GitHubIcon}
-            alt="Alex's GitHub profile and projects"
-            className="nav-icon"
-          ></img>
-        </a>
-        <a
-          className="navbar-brand"
-          href="https://gitlab.com/users/awagner50/projects"
-        >
-          <img
-            src={GitLabIcon}
-            alt="Alex's GitLab profile and projects"
-            className="nav-icon"
-          ></img>
-        </a>
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+      <div className="navbar-containter">
+        <div className="container-field">
+          <NavLink
+            to="/"
+            end
+            className={({ isActive }) =>
+              `navbar-brand ${isActive ? "active-link" : ""}`
+            }
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/school-projects"
+            className={({ isActive }) =>
+              `navbar-brand ${isActive ? "active-link" : ""}`
+            }
+          >
+            School Projects
+          </NavLink>
+          <NavLink
+            to="/work-experience"
+            className={({ isActive }) =>
+              `navbar-brand ${isActive ? "active-link" : ""}`
+            }
+          >
+            Work Experience
+          </NavLink>
+        </div>
+        <div className="container-field">
+          <a
+            className="navbar-brand"
+            href="https://www.linkedin.com/in/alexander-m-wagner/"
+          >
+            <img
+              src={LinkedInIcon}
+              alt="Alex's Linkedin profile page"
+              className="nav-icon"
+            ></img>
+          </a>
+          <a className="navbar-brand" href="https://github.com/alexw144">
+            <img
+              src={GitHubIcon}
+              alt="Alex's GitHub profile and projects"
+              className="nav-icon"
+            ></img>
+          </a>
+          <a
+            className="navbar-brand"
+            href="https://gitlab.com/users/awagner50/projects"
+          >
+            <img
+              src={GitLabIcon}
+              alt="Alex's GitLab profile and projects"
+              className="nav-icon"
+            ></img>
+          </a>
+        </div>
       </div>
     </nav>
   );
