044d5dfd1515bdb096dbacab6d8b5a198b5bc837
alexw144 <[amwagner4472@live.carlow.edu])>
2025-10-21 14:54:47 -0400

    Changed hash router to browse router. Then I created a file in public for redirects. This fix a "/#/" being in the url address.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { SchoolProjects } from "./pages/SchoolProjects";
 import { WorkExperience } from "./pages/WorkExperience";
